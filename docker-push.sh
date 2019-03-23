d1b686c1ce50bc58bc281d80c577ebdf2010a506
Thomas Michelet <thomas.michelet@wifirst.fr>
2019-03-23 21:13:24 +0100

    Push docker with branche name on tag

--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -f Dockerfile-prod -t "rudeboytom/koalattitude:$1" .
+docker build -f Dockerfile-prod -t "rudeboytom/koalattitude:$TRAVIS_BRANCH-$1" .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
-docker push "rudeboytom/koalattitude:$1"
+docker push "rudeboytom/koalattitude:$TRAVIS_BRANCH-$1"
