4ef05e817dc6c0df8ef34f7aa0ee52a59cecbb7c
Thomas Michelet <thomas.michelet@wifirst.fr>
2019-03-23 21:34:03 +0100

    Replace -u by --username

--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -f Dockerfile-prod -t "rudeboytom/koalattitude:$TRAVIS_BRANCH-$1" .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin docker.io
 docker push "rudeboytom/koalattitude:$TRAVIS_BRANCH-$1"
